ed6ac218deac887ba3d2b9c0112e744a1ff71fba
Gaston Gayan <78116020+gastongayan@users.noreply.github.com>
2022-04-18 09:35:57 -0300

    Add files via upload

---
+++ Trabajo_Practico_Nro1/Biblioteca/Calculadora.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Calculadora
+    {
+        /// <summary>
+        /// Valida que el operador recibido por parametro sea igual a * / - +, caso contrario retorna +
+        /// </summary>
+        /// <param name="operador"></param>
+        /// <returns>operador validado</returns>
+        private static char ValidarOperador(char operador)
+        {
+            if (operador == '*' || operador == '/' || operador == '+' || operador == '-')
+            {
+                return operador;
+            }
+            return '+';
+        }
+
+        /// <summary>
+        /// Reliza las operaciones solicitadas por el formulario, utilizando las sobrecarga de los operadores
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="operador"></param>
+        /// <returns>resultado de la operacion matematica</returns>
+        public double Operar(Operando num1, Operando num2, string operador)
+        {
+            double resultado = 0;
+           if (num1 != null && num2 != null)
+           {
+                switch (operador)
+                {
+                    case "+":
+                        resultado = num1 + num2;
+                        break;
+                    case "-":
+                        resultado = num2 - num1;
+                        break;
+                    case "*":
+                        resultado = num1 * num2;
+                        break;
+                    case "/":
+                        resultado = num1 / num2;
+                        break;
+                }
+           }
+            return resultado;
+        }
+    }
+
+   
+}
